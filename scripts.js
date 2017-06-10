c5375d479eef4bed49be9d544d4fb1f399219202
Wiktoria <wpawlik73@gmail.com>
2017-06-10 09:42:10 +0200

    test1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+var span = $("span");
+  span.each(function(index, element) {
+    if (index % 2 == 0) {
+      $(element).css('color', 'blue');
+    }
+  });
+
+// guzik
+
+var paragraph = $('p');
+paragraph.each(function(index, element) {
+  var newButton = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+
+  $(element).append(newButton)
+});
+
+// event
+$("button").click(function() {
+  alert($(this).attr(data-tmp));
+})
 
