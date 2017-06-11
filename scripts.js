f758d5a23af08a69c70939f1adcaf11c39007e5a
Wiktoria <wpawlik73@gmail.com>
2017-06-11 12:14:06 +0200

    test3

--- scripts.js
+++ scripts.js
@@ -1,25 +1,14 @@
 $(document).ready(function() {
-  console.log('Ready to work');
+  
+	var paragraph = $('p');
+	paragraph.each(function(index, element) {
+	  var newButton = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+
+	  $(element).append(newButton)
+	});
+
+	$("button").click(function() {
+	  alert($(this).attr(data-tmp));
+	})
+
 });
-
-var span = $("span");
-  span.each(function(index, element) {
-    if (index % 2 == 0) {
-      $(element).css('color', 'blue');
-    }
-  });
-
-// guzik
-
-var paragraph = $('p');
-paragraph.each(function(index, element) {
-  var newButton = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-
-  $(element).append(newButton)
-});
-
-// event
-$("button").click(function() {
-  alert($(this).attr(data-tmp));
-})
-
