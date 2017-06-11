853a33b49386a7497edf364c77d897e962a7f4bd
Wiktoria <wpawlik73@gmail.com>
2017-06-11 12:29:26 +0200

    test5

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+	var span = $("span");
+    span.each(function(index, element) {
+      if (index % 2 == 0) {
+        $(element).css('color', 'blue');
+      }
+    });
+		
 	var paragraph = $('p');
 	paragraph.each(function(index, element) {
 	  var newButton = '<button class="btn" data-tmp="' + index + '">Click me</button>'
