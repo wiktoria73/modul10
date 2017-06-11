a458c18ff376a2cb2675d99d67aff37a43de8c44
Wiktoria <wpawlik73@gmail.com>
2017-06-11 12:18:06 +0200

    test1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function() {
+  var carouselList = $('#carousel ul');
+
+  function changeSlide() {
+    carouselList.animate({'marginLeft':'-500'}, 500, function() {
+      var firstItem = carouselList.find("li:first");
+      var lastItem = carouselList.find("li:last");
+      lastItem.after(firstItem);
+      carouselList.css({marginLeft:0});
+  });}
+  setInterval(changeSlide, 3000);
+});
+
